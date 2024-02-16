2dc9006f2b4dad8f57229c44f6be4323bbcd9820
Bogomil Petrov <bogipetrov@gmail.com>
2024-02-16 13:52:30 +0200

    adding a starRating component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import App from "./App";
+import StarRating from "./StarRating";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    {/* <StarRating maxRating={10} /> */}
+    <Test />
+    <StarRating
+      maxRating={5}
+      color="red"
+      size={54}
+      messages={["MY CHOICEEEE!", "Asexual", "Mrusen gender", "Bi", "Str8"]}
+    />
   </React.StrictMode>
 );
